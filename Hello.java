400f1b41b701d8a1eb3fc49658641a60ec726baf
jamal <jamal.brotecs@gmail.com>
2019-07-16 08:53:37 +0600

    Change greeting method

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Vs Code is Awesome..");
+        greeting("Jamal Khan");
+    }
+
+    public static void greeting(String name){
+        System.out.println("Welcome " + name);
     }
 }
