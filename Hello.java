d6b81370889883b4e34796b994f81252e0a9b3d5
jamal <jamal.brotecs@gmail.com>
2019-07-16 09:16:45 +0600

    Add new method called sqr

--- Hello.java
+++ Hello.java
@@ -2,4 +2,8 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Vs Code is Awesome..");
     }
+
+    public static int sqr(int x){
+        return x*x;
+    }
 }
