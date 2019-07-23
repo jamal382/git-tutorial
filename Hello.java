c7f2a9768a3fea72581502aaa7472863440e96f4
jamal <jamal.brotecs@gmail.com>
2019-07-23 09:21:47 +0600

    delete previous method and add new method called sqr

--- Hello.java
+++ Hello.java
@@ -1,15 +1,10 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Vs Code is Awesome..");
-        message();
     }
 
-
-    public static void message(){
-        System.out.println("Hurray !! now we are able to contribute on open source ..");
+    public static sqr(int x){
+        return x*x;
     }
-
-    public static add(int a, int b){
-        return a+b;
-    }
+    
 }
